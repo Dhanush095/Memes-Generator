727586d7be72007dcad7b9af4968a857197c8762
Dhanush095 <dhanushkumarr078@gmail.com>
2024-09-12 13:51:29 +0530

    changed name

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
                 src="./images/troll-face.png" 
                 className="header--image"
             />
-            <h2 className="header--title">Dhanush Meme Generator</h2>
+            <h2 className="header--title">Dhanush kumar Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
